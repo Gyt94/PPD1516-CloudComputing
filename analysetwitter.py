99c49bc53ad9a606aab878b209a727ee1cbb78b9
doclapin <sim_conejo@yahoo.fr>
2016-02-05 15:58:36 +0100

    Màj script twitter

--- analysetwitter.py
+++ analysetwitter.py
@@ -1,55 +1,53 @@
-import tweepy
-import json
-import os, sys
+import json, tweepy, os, sys, getopt, codecs
+import config
 from azure.servicebus import ServiceBusService
 from tweepy import OAuthHandler
 from time import sleep
 
 #Variables de configuration
 
-servns = 'Miage2016' # service_bus _namespace
-key_name = 'manage'
-key_value = 'KEOU/Qun1wrPFO8eLXM4HCEa59WDP2qLcuuhNapYEjQ='  # SharedAccessKey from Azure portal
-sbs = ServiceBusService(service_namespace=servns,
-                        shared_access_key_name=key_name,
-                        shared_access_key_value=key_value) # Create a ServiceBus Service Object
+sbs = ServiceBusService(service_namespace=config.servns,
+                        shared_access_key_name=config.key_name,
+                        shared_access_key_value=config.key_value) # Create a ServiceBus Service Object
 
-access_token = '4832196184-FY45HwZRH3d9fNFAcc8tbdwBonqAoPNtCmaQX3C'
-access_secret = '3i271smaOXbpDJ33mSzql90uHQXXe3POnPIWxkq3zirML'
-consumer_key = 'njSeydg32gT3ezDvCNrn584CB'
-consumer_secret = 'S2uj20xoQXW3Ae35MGRMgPmuv2KgNwlFjrhXLgpHIKKUthrclM'
-
-query = ''
 max_tweets = 20
-tweet_id = 1000
-#Fin Variables de configuration
 
-#Code 
+arg_query = ''
+arg_tweet_id = 10000
+arg_time = 60
 
-# query = sys.argv[1]
-
-auth = OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_secret)
+auth = OAuthHandler(config.consumer_key, config.consumer_secret)
+auth.set_access_token(config.access_token, config.access_secret)
  
 api = tweepy.API(auth)
 
-# print(type(tweepy.Cursor(api.home_timeline))
-	
+opts, args = getopt.getopt(sys.argv[1:],'q:t:')
+for opt, arg in opts:
+	# en travaux : changer entre mode requete et timeline
+   if opt == '-q':
+      arg_query = arg
+   elif opt == '-t':
+      arg_time = float(arg)
+
+# if arg_query == '':
+tweet_id = arg_tweet_id
 while True:
 	i = 0
 	for status in tweepy.Cursor(api.home_timeline, since_id = tweet_id).items(max_tweets):
 		if i == 0:
 			last_tweet_id = status.id
-		searched_tweets = status._json
-		sbs.send_event('iot', searched_tweets)
+		# print(unicode(status.text))
 		print("tweet : " + str(status.id))
+		searched_tweets = "{ 'source': '" + unicode(status.user.name) + "' , 'text': '" + unicode(status.text) + "' }"
+		print(unicode(searched_tweets))
+		sbs.send_event('iot', unicode(searched_tweets))
 		i = i + 1
 
 	tweet_id = last_tweet_id
 	print('dodo' + ' id : ' + str(last_tweet_id))
-	sleep(10)
+	sleep(arg_time)
 
-
+# else:
 # searched_tweets = [status._json for status in tweepy.Cursor(api.home_timeline).items(max_tweets)]
 # searched_tweets = json.dumps(searched_tweets)
 # sbs.send_event('iot', searched_tweets)
