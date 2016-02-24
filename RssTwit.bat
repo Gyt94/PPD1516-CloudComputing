f3f38cb22df27f3d137cf8296ead05339afa8024
Gyt94 <gygy.tho@gmail.com>
2016-02-24 10:52:51 +0100

    Application
    ajout de l'application .bat

---
+++ RssTwit.bat
@@ -0,0 +1,1 @@
+C:\Python27\python.exe C:\Users\Guillaume\Documents\GitHub\PPD1516-CloudComputing\interface.py %*
---
+++ interface.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from Tkinter import *
+
+class App:
+
+    def __init__(self, master):
+
+        frame = Frame(master, height=768, width=576, cursor="hand2")
+        frame.pack()
+        img = PhotoImage(file="Logo-Paris-Descartes.gif")
+        lab=Label(frame, image=img)
+        lab.photo = img
+        lab.pack(side=TOP)
+        self.button = Button(
+            frame, text="QUIT", fg="red", command=frame.quit
+            )
+        self.button.pack(side=LEFT)
+
+        self.hi_there = Button(frame, text="Hello", command=self.say_hi)
+        self.hi_there.pack(side=LEFT)
+
+    def say_hi(self):
+        print "hi there, everyone!"
+
+root = Tk()
+root.geometry("500x500")
+app = App(root)
+
+root.mainloop()
+root.destroy() # optional; see description below
